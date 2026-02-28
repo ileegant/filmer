c88305251e4bafd86457796086dacdc900aa9ef5
ileegant <osukhoruchko@gmail.com>
2026-02-28 17:11:08 +0200

    create function to fetch movies from imdb api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { fetchMovies } from "./api/tmdb";
 
 function App() {
-  return <></>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies(1).then((data) => {
+      setMovies(data);
+    });
+  }, []);
+
+  return (
+    <>
+      {movies.map((i) => (
+        <>
+          <h1>Movie {i.title}</h1>
+          <div>{JSON.stringify(i)}</div>
+        </>
+      ))}
+    </>
+  );
 }
 
 export default App;
